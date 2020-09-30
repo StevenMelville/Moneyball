71364d6fd56e74a7bdc69c5ecef82f9a4a5de644
Steven Melville <stevenmelville42@gmail.com>
2020-09-30 15:39:01 -0600

    Added Player class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.io.File;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        loadPlayers();
+    }
 
+    public static ArrayList<Player> loadPlayers(){
+        ArrayList<Player> list = new ArrayList<>();
+
+        try {
+            Scanner file = new Scanner(new File("all_seasons.csv"));
+            String player;
+            String info[];
+            file.nextLine();
+
+            while(file.hasNextLine()){
+                player = file.nextLine();
+                info = player.split(",");
+
+                list.add(new Player());
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,18 @@
+public class Player {
+    private String name;
+    private String team;
+    private int age;
+    private double height;
+    private double weight;
+    private String college;
+    private String country;
+    private int draftYear;
+    private int draftRound;
+    private int gamesPlayed;
+    private double averagePoints;
+    private double averageRebounds;
+
+    public Player(){
+
+    }
+}
