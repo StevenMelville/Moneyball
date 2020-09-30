86b9ba83a3727ce9e420bbcec43f1ba3b7579f6c
Steven Melville <stevenmelville42@gmail.com>
2020-09-30 15:51:23 -0600

    Finished Player Class

--- src/Player.java
+++ src/Player.java
@@ -11,8 +11,98 @@ public class Player {
     private int gamesPlayed;
     private double averagePoints;
     private double averageRebounds;
+    private double averageAssists;
+    private double netRating;
+    private double offensiveReboundPercentage;
+    private double defensiveReboundPercentage;
+    private double teamPlayUsagePercentage;
+    private double shootingEfficiencyPercentage;
+    private double assistPercent;
+    private String nbaSeason;
 
     public Player(){
 
     }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getAssistPercent() {
+        return assistPercent;
+    }
+
+    public double getAverageAssists() {
+        return averageAssists;
+    }
+
+    public double getAveragePoints() {
+        return averagePoints;
+    }
+
+    public double getAverageRebounds() {
+        return averageRebounds;
+    }
+
+    public double getDefensiveReboundPercentage() {
+        return defensiveReboundPercentage;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getNetRating() {
+        return netRating;
+    }
+
+    public double getOffensiveReboundPercentage() {
+        return offensiveReboundPercentage;
+    }
+
+    public double getShootingEfficiencyPercentage() {
+        return shootingEfficiencyPercentage;
+    }
+
+    public double getTeamPlayUsagePercentage() {
+        return teamPlayUsagePercentage;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getDraftRound() {
+        return draftRound;
+    }
+
+    public int getDraftYear() {
+        return draftYear;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNbaSeason() {
+        return nbaSeason;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+
 }
